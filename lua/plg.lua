05951c06fce3cabfd5419a71b91212cc64ef6f8d
davidtongeo <david1108n@gmail.com>
2023-11-27 13:36:55 -0500

    added noice.nvim

--- lua/plg.lua
+++ lua/plg.lua
@@ -143,10 +143,31 @@ require("lazy").setup({
 		"Hoffs/omnisharp-extended-lsp.nvim",
 	},
 	{
-		"folke/neodev.nvim",
+		"folke/noice.nvim",
 		opts = {},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
 		config = function()
-			require("neodev").setup({})
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
 		end,
 	},
 	-- Langs related
