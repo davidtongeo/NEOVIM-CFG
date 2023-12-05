1df519af79b509ccffd04c9cdd4f579ed199307f
davidtongeo <david1108n@gmail.com>
2023-12-05 17:07:56 -0500

    cmp fix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -16,8 +16,8 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-j>"] = cmp.mapping.select_prev_item(),
-				["<C-k>"] = cmp.mapping.select_next_item(),
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
