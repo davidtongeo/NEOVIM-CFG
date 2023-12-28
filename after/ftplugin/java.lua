c7a4d02a5abc2f8342562da2bd3aaa49f74e2743
davidtongeo <david1108n@gmail.com>
2023-12-27 20:07:58 -0500

    ultim

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -126,3 +126,10 @@ local config = {
 	on_attach = base.on_attach,
 }
 require("jdtls").start_or_attach(config)
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
