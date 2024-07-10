702523ce1284603bcad614852fa0fd538d80a487
davidtongeo <david1108n@gmail.com>
2024-07-10 12:51:34 -0500

    last changes

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,135 +1,33 @@
+local path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
 local base = require("plugins.lsp.base")
---local path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
-local path = os.getenv("HOME") .. "/git_clone/jdtls"
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-
-local workspace_dir = os.getenv("HOME")
-	.. "/.local/share/eclipse/"
-	.. vim.fn.fnamemodify(require("jdtls.setup").find_root({ "gradlew", "mvnw", ".git" }), ":p:h:t")
+--local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local config = {
-	-- The command that starts the language server
-	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-	cmd = {
-
-		-- 💀
-		"java", -- or '/path/to/java17_or_newer/bin/java'
-		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
-		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-		"-Dosgi.bundles.defaultStartLevel=4",
-		"-Declipse.product=org.eclipse.jdt.ls.core.product",
-		"-Dlog.protocol=true",
-		"-Dlog.level=ALL",
-		"-Xmx1g",
-		"--add-modules=ALL-SYSTEM",
-		"--add-opens",
-		"java.base/java.util=ALL-UNNAMED",
-		"--add-opens",
-		"java.base/java.lang=ALL-UNNAMED",
-
-		-- 💀
-		"-jar",
-		--path .. "\\plugins\\org.eclipse.equinox.launcher_1.6.500.v20230622-2056.jar",
-		path .. "/plugins/org.eclipse.equinox.launcher_1.6.600.v20231106-1826.jar",
-		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-		-- Must point to the                                                     Change this to
-		-- eclipse.jdt.ls installation                                           the actual version
-
-		-- 💀
-		"-configuration",
-		--path .. "\\config_win",
-		path .. "/config_linux",
-		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-		-- Must point to the                      Change to one of `linux`, `win` or `mac`
-		-- eclipse.jdt.ls installation            Depending on your system.
-
-		-- 💀
-		-- See `data directory configuration` section in the README
-		"-data",
-		workspace_dir,
-	},
-
-	-- 💀
-	-- This is the default if not provided, you can remove it. Or adjust as needed.
-	-- One dedicated LSP server & client will be started per unique root_dir
-	root_dir = require("jdtls.setup").find_root({ "gradlew", ".git", "mvnw" }),
-
-	-- Here you can configure eclipse.jdt.ls specific settings
-	-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-	-- for a list of options
-	settings = {
-		java = {
-			signatureHelp = { enabled = true },
-			contentProvider = { preferred = "fernflower" }, -- Use fernflower to decompile library code
-			-- Specify any completion options
-			completion = {
-				favoriteStaticMembers = {
-					"org.hamcrest.MatcherAssert.assertThat",
-					"org.hamcrest.Matchers.*",
-					"org.hamcrest.CoreMatchers.*",
-					"org.junit.jupiter.api.Assertions.*",
-					"java.util.Objects.requireNonNull",
-					"java.util.Objects.requireNonNullElse",
-					"org.mockito.Mockito.*",
-				},
-				filteredTypes = {
-					"com.sun.*",
-					"io.micrometer.shaded.*",
-					"java.awt.*",
-					"jdk.*",
-					"sun.*",
-				},
-			},
-			-- Specify any options for organizing imports
-			sources = {
-				organizeImports = {
-					starThreshold = 9999,
-					staticStarThreshold = 9999,
-				},
-			},
-			-- How code generation should act
-			codeGeneration = {
-				toString = {
-					template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-				},
-				hashCodeEquals = {
-					useJava7Objects = true,
-				},
-				useBlocks = true,
-			},
-			-- If you are developing in projects with different Java versions, you need
-			-- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
-			-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-			-- And search for `interface RuntimeOption`
-			-- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
-			configuration = {
-				runtimes = {
-					{
-						name = "JavaSE-21",
-						path = "/usr/lib/jvm/java-21-openjdk/",
-					},
-				},
-			},
-		},
-	},
-
-	-- Language server `initializationOptions`
-	-- You need to extend the `bundles` with paths to jar files
-	-- if you want to use additional eclipse.jdt.ls plugins.
-	--
-	-- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-	--
-	-- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-	init_options = {
-		bundles = {},
-	},
-	on_attach = base.on_attach,
+  cmd = { path .. "/bin/jdtls" },
+  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  capabilities = base.capabilities,
+  on_attach = base.on_attach,
+  settings = {
+    java = {
+      signatureHelp = {
+        enabled = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all',
+        },
+      },
+      configuration = {
+        -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+        -- And search for `interface RuntimeOption`
+        -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = "/usr/lib/jvm/java-21-openjdk/",
+          },
+        }
+      }
+    }
+  }
 }
-require("jdtls").start_or_attach(config)
-
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.termguicolors = true
+require('jdtls').start_or_attach(config)
--- lua/essentials.lua
+++ lua/essentials.lua
@@ -4,11 +4,11 @@ vim.opt.clipboard:append({ "unnamedplus" }) -- Clipboard
 vim.opt.nu = true
 vim.opt.scrolloff = 8
 vim.opt.relativenumber = true
-
+vim.opt.pumheight = 6
 -- Tabs as method of indenting
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
