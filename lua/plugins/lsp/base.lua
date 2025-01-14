c9b2ec8046d33ea178751f32ae7f727dfb9beabc
davidtongeo <david1108n@gmail.com>
2025-01-13 19:34:44 -0500

    adding keybinds and fixing ts lsp

--- lua/plugins/lsp/base.lua
+++ lua/plugins/lsp/base.lua
@@ -1,24 +1,26 @@
 local M = {}
 --on_attach for server setups
 M.on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  -- local function buf_set_option(...)
-  -- 	vim.api.nvim_buf_set_option(bufnr, ...)
-  -- end
-  --
-  -- --Enable completion triggered by <c-x><c-o>
-  -- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+	-- local function buf_set_option(...)
+	-- 	vim.api.nvim_buf_set_option(bufnr, ...)
+	-- end
+	--
+	-- --Enable completion triggered by <c-x><c-o>
+	-- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "<leader>la", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  vim.lsp.inlay_hint.enable(true)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "<leader>lw", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "<leader>lr", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>la", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	vim.lsp.inlay_hint.enable(true)
 end
 --capabilities
 M.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,23 +1,23 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-        { "folke/neodev.nvim",  opts = {} },
-    },
-    opts = function()
-        local base = require("plugins.lsp.base")
-        return {
-            capabilities = base.capabilities,
-            on_attach = base.on_attach,
-        }
-    end,
-    config = function(_, opts)
-        local lspconfig = require("lspconfig")
-        -- setups
-        lspconfig.lua_ls.setup(opts)
-        lspconfig.csharp_ls.setup(opts)
-        lspconfig.html.setup(opts)
-        lspconfig.tsserver.setup(opts)
-        lspconfig.cssls.setup(opts)
-    end,
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	opts = function()
+		local base = require("plugins.lsp.base")
+		return {
+			capabilities = base.capabilities,
+			on_attach = base.on_attach,
+		}
+	end,
+	config = function(_, opts)
+		local lspconfig = require("lspconfig")
+		-- setups
+		lspconfig.lua_ls.setup(opts)
+		lspconfig.csharp_ls.setup(opts)
+		lspconfig.html.setup(opts)
+		lspconfig.ts_ls.setup(opts)
+		lspconfig.cssls.setup(opts)
+	end,
 }
