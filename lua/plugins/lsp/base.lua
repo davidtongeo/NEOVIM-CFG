0d22cd026916fbb3fe20801057144b7876776f7c
davidtongeo <david1108n@gmail.com>
2025-03-29 22:50:59 -0500

    latest

--- lua/plugins/lsp/base.lua
+++ lua/plugins/lsp/base.lua
@@ -12,7 +12,8 @@ M.on_attach = function(client, bufnr)
 	-- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 	-- Mappings.
 	local opts = { noremap = true, silent = true }
-
+	-- enable native vim lsp
+	--vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	buf_set_keymap("n", "<leader>lw", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
@@ -20,8 +21,11 @@ M.on_attach = function(client, bufnr)
 	buf_set_keymap("n", "<leader>la", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+
 	vim.lsp.inlay_hint.enable(true)
 end
 --capabilities
-M.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+--M.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+M.capabilities = require("blink.cmp").get_lsp_capabilities()
+
 return M
