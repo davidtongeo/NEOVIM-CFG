66030ad7240fcdae4b50c9d392f586cf11a6caaa
david <david1108n@gmail.com>
2025-12-06 18:04:12 -0500

    dap mod

--- plugin/nvim-dap.lua
+++ plugin/nvim-dap.lua
@@ -4,4 +4,21 @@ MiniDeps.add({
 })
 
 local dap = require("dap")
-require("dapui").setup()
+local dapui = require("dapui")
+dapui.setup()
+
+--keybinds
+-- Open DAP UI
+vim.keymap.set('n', '<leader>do', function()
+  dapui.open()
+end, { desc = "Open DAP UI", noremap = true, silent = true })
+
+-- Close DAP UI
+vim.keymap.set('n', '<leader>dc', function()
+  dapui.close()
+end, { desc = "Close DAP UI", noremap = true, silent = true })
+
+-- Toggle breakpoint
+vim.keymap.set('n', '<leader>db', function()
+  dap.toggle_breakpoint()
+end, { desc = "Toggle breakpoint", noremap = true, silent = true })
